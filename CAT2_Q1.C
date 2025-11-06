b066a0fcdab135bd82b79548ad8970a74ee9b8fc
Lynn06tech <wamagatalynn@gmail.com>
2025-11-06 11:46:35 +0300

    Create CAT2_Q1.C

---
+++ CAT2_Q1.C
@@ -0,0 +1,24 @@
+/*
+Name:Wamagata Lynn Njeri
+Reg.no:CT101/G/26423/25
+*/
+#include <stdio.h>
+
+int main() {
+    int scores[4][2] = {
+        {65, 92},
+        {84, 72},
+        {35, 70},
+        {59, 67}
+    };
+
+    printf("The elements of the array are:\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d\t", scores[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
